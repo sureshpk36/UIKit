1fe4f5fe4a829fe96302d01e90d746eaeb2643d4
Suresh PK <141796572+sureshpk36@users.noreply.github.com>
2025-06-11 14:58:09 +0530

    Update 1 - UIViews.swift

--- 1 - UIViews.swift
+++ 1 - UIViews.swift
@@ -11,55 +11,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // MARK: - Main View Setup
         view.backgroundColor = .green
+
+        // MARK: - Blue Container View (myview)
         let myview = UIView(frame: CGRect(x: 50, y: 50, width: 200, height: 100))
         myview.backgroundColor = .blue
-        
-        
+        myview.clipsToBounds = true                       // Ensures subviews don’t overflow
+        myview.transform = CGAffineTransform(rotationAngle: .pi / 4) // Rotate 45 degrees
+
+        // Orange Subview inside myview
         let secondview = UIView(frame: CGRect(x: 50, y: 50, width: 200, height: 100))
-        
-        myview.addSubview(secondview)
-        view.addSubview(myview)
         secondview.backgroundColor = .orange
-        
-        myview.clipsToBounds = true
-        
-        myview.transform = CGAffineTransform(rotationAngle: .pi/4)
-        
-        let label = UILabel(frame:  CGRect(x: 110, y: 400, width: 200, height: 100))
+        myview.addSubview(secondview)
+
+        view.addSubview(myview)
+
+        // MARK: - UILabel
+        let label = UILabel(frame: CGRect(x: 110, y: 400, width: 200, height: 100))
         label.text = "Hello, World!"
         label.textColor = .black
         label.textAlignment = .center
         view.addSubview(label)
-        
-        
+
+        // MARK: - System Image (SF Symbol)
         let image = UIImageView(frame: CGRect(x: 100, y: 200, width: 200, height: 200))
-        image.image = UIImage(systemName: "apple.logo")
+        image.image = UIImage(systemName: "apple.logo")  // SF Symbol image
         image.contentMode = .scaleAspectFit
+        image.alpha = 0.5                                 // Semi-transparent
         view.addSubview(image)
-        
-        image.alpha = 0.5
-        // image.isHidden = true
-        
-        
+
+        // MARK: - Asset Image (Local)
         let img2 = UIImageView(frame: CGRect(x: 100, y: 400, width: 200, height: 200))
-        img2.image = UIImage(named:"apple")
-        view.addSubview(img2)
+        img2.image = UIImage(named: "apple")             // From Assets.xcassets
         img2.contentMode = .scaleAspectFit
-        
-        
-        
-        
-        
-        
-        
+        view.addSubview(img2)
     }
-
-
 }
-
-
-
-
-
